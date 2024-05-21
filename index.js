b0f081ceb336edbc001b11084d4c6f9f0f57815f
Max Kohler <hello@maxkohler.com>
2024-05-21 14:23:07 +0200

    Disable sandbox

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const now = new Date()
 const ts = now.toLocaleDateString("en-uk", { dateStyle: "short" }).replace(/\//g, "-")
 
 ;(async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  })
+
   const page = await browser.newPage()
   await page.setViewport({ width: 1500, height: 1000 })
 
